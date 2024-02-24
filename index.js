84f9d8b83372ae24a723eaf397695f9b0e483565
Marchi <marchig314@gmail.com>
2024-02-23 21:27:21 -0300

    papad

--- index.js
+++ index.js
@@ -1,68 +1,40 @@
-const aa = document.querySelector(".aa")
-const ab = document.querySelector(".ab")
-const ba = document.querySelector(".ba")
-const bb = document.querySelector(".bb")
-const body = document.querySelector('body')
-
-const colorArray = [aa,ab,ba,bb]
-
-/* 
-
-click to start
-
-random stage1 color picked and stored 
-listener stage1
-se click =/ stage -> game over
-se cliock stage1 -> pisca stage1 + pisca staged 2
-...
-
-it can be done like:
-
-listener any of matriz -> pick random color + flick + push array + add listener if click right + add wrong game over others
-
-if click correct -> remove event listener + flick array[0] + random color flick and push array + add array[0] event listener
-+ if click array[0], array[0] remove listener and add game over, array[1] add listener and remove game over
-....
-if click gameover -> title screen + array clear ez
+const aa = document.querySelector(".aa");
+const ab = document.querySelector(".ab");
+const ba = document.querySelector(".ba");
+const bb = document.querySelector(".bb");
+const button = document.querySelector(".button")
+const letitle = document.querySelector(".letitle")
+const grido = document.querySelector(".box")
 
 
-pick random color + add array + add corret func + add other wrong func
-correct -> flick corrct + click correct + flick correct + flick random
-
-*/
-
-function randomColor (){
-    return Math.round(Math.random()*4)
-}
+const colorArray = [aa, ab, ba, bb];
+let gamerOrder = [];
+let playerClick = [];
 
 
-function correct(element) {
-    element.classList.add("flick");
-    setTimeout(() => {
-      element.classList.remove("flick");
-    }, 1000);
-  }
+var GameStart = false
 
+/*Button*/
+button.addEventListener("click", ()=>{
 
-function GameStart (){
-    
-
- let GamerOrder = [];
-
- function Correct () {
-    let random = randomColor()
-    GamerOrder.push(colorArray[random])
-    correct(colorArray[random])
-    
- }
- 
-Correct()
   
-colorArray.forEach(function (element){
-    element.removeEventListener("click", Start)
- })
+  [button,letitle].forEach((element)=>{
+    element.style.display = "none"
+    element.style.zIndex = "-1"
+  })
 
-setTimeout(colorArray[random].classList.remove("flick"), 3000);
+    grido.style.display = "grid"
+   GameStart = true
+})
+
+document.querySelector(".button").addEventListener("click", Gaming)
+
+/*Button*/
+
+
+
+function randomColor() {
+  return Math.floor(Math.random() * 4);
 }
 
 
@@ -71,12 +43,45 @@ setTimeout(colorArray[random].classList.remove("flick"), 3000);
 
 
 
-function Start (){
-    GameStart()
+
+
+
+
+
+
+
+
+function Gaming (){
+  playerClick = [];
+  
+  let randomcu = randomColor()
+  gamerOrder.push(colorArray[randomcu])
+
+  FlickSequence()
 }
 
-colorArray.forEach(function (element){
-    element.addEventListener("click", Start)
- })
 
 
+
+
+
+
+
+/* Flicking */
+function FlickSequence (){
+  gamerOrder.forEach(function(element){
+    element.classList.add("flick")
+
+  })
+}
+
+/* Flicking */
+
+
+function Player (){
+  document.querySelectorAll(".opt").forEach(element =>{
+    element.addEventListener("click",()=>{
+     playerClick.push(this)
+    })
+  })
+}
